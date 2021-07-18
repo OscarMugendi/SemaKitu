4a32aca8d60cbe7128828656bd95dbb9f1ef176a
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-07-19 00:40:41 +0300

    Generate quote-form compnent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadingComponent } from './heading/heading.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadingComponent
+    HeadingComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
