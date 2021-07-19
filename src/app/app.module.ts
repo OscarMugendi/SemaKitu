4ce53d892b7433bf8563cbc3da799114e08151ce
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-07-19 06:42:08 +0300

    HTML and CSS reset.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
-import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteFormComponent,
-    QuoteComponent,
-    QuoteDetailsComponent,
   ],
   imports: [
     BrowserModule,
