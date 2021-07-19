8e1f4742a427eacc2bed87bbd2e9833531ae5ae3
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-07-19 08:06:44 +0300

    HTML and CSS edits for details component.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-details',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+
+  @Input() name!: Quote;
+
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
+ 
+  @Output() myLike = new EventEmitter<boolean>();
+
+  upVote(yes:boolean){
+    this.myLike.emit(yes);
+  }
+
+
+  @Output() yourLike = new EventEmitter<boolean>();
+
+  downVote(no:boolean){
+    this.yourLike.emit(no);
+  }
 
   constructor() { }
 
