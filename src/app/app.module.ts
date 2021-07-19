64f424fd58feeef533045c5ab22d014005fc91e4
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-07-19 08:12:44 +0300

    Generate Form component; Basic HTML and CSS rules and styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteComponent } from './quote/quote.component';
 import { DetailsComponent } from './details/details.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent,
     QuoteComponent,
     DetailsComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
